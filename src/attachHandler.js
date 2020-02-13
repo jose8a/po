65b686928adc315d105d52ecfd6b84cf060201e0
jose8a <ochovilla@gmail.com>
2020-02-13 07:39:21 -0800

    Partial, broken code changes must be versioned in order to move to new laptop

--- src/attachHandler.js
+++ src/attachHandler.js
@@ -1,4 +1,5 @@
-#!/usr/bin/env node
+const sh = require('shelljs');
+const chalk = require('chalk');
 
 /*
   po_attach_session() {
@@ -7,16 +8,28 @@
   }
 */
 
+const { exec, spawn } = require('child_process');
+
+function onAttach(session, cmd) {
+  sh.exec(`tmux attach-session -t ${session}`);
+  // ----- const tmSession = exec(`tmux attach-session -t ${session}`, (err, stdout, stderr) => {
+  // -----   if (err) {
+  // -----     console.error(`exec error: ${err}`);
+  // -----     return;
+  // -----   }
+
+  // -----   console.log(`Number of files ${stdout}`);
+  // ----- });
+
+  // --- tmSession.on('error', (err) => {
+  // ---   console.log('Failed to start subprocess.');
+  // --- });
+}
+
 module.exports = function ( program ) {
   program
     .command('attach <session>')
     .alias('a')
     .description('re-attach to a background tmux session')
-    // --- .option("-s, --setup_mode [mode]", "Which setup mode to use")
-    .action(function(env, options){
-      // --- var mode = options.setup_mode || "normal";
-      // --- env = env || 'all';
-      // --- console.log('setup for %s env(s) with %s mode', env, mode);
-      console.log(`Attaching ...`);
-    });
+    .action(onAttach);
 }
--- src/openHandler.js
+++ src/openHandler.js
@@ -1,12 +1,7 @@
-#!/usr/bin/env node
+const sh = require('shelljs');
+const chalk = require('chalk');
 
 /*
-
-  po_open_one() {
-    echo "opening session $SESSION ..."
-    tmuxinator $SESSION
-  }
-
   po_open_group () {
     FILE="$PO_GROUPS/$GROUP.list"
 
@@ -15,24 +10,56 @@
   }
 
   po_open_restart_list ==> cat $RESTART_FILE | xargs -n 1 tmuxinator start
-
 */
+
+const { fork, exec, execFile, spawn } = require('child_process');
+const fs = require('fs');
+
+function onOpen(env, options) {
+  if (options.project) {
+    console.log(`project: ${options.project}`);
+    // check if tmuxinator config exists
+    if (fs.existsSync(`~/.tmuxinator/${options.project}.yml`)) {
+      console.log(chalk.red(`The tmuxinator configuration does not exist.`));
+      console.log();
+      return;
+    }
+
+    const session = spawn('sh', [`tmuxinator`, `start`, `${options.project}`], {
+      detached: true,
+      stdio: 'ignore'
+    });
+
+    session.unref();
+
+    session.on('error', (err) => {
+      console.log('Failed to start subprocess.');
+    });
+
+    return;
+  }
+
+  if (options.project_group) {
+    console.log(`project-group: ${options.project_group}`);
+    console.warn(`TODO: this option not yet implemented.`);
+    return;
+  }
+
+  if (options.restart_group) {
+    console.log(`isRestart?: ${options.restart_group}`);
+    return;
+  }
+
+  console.log(`Error - wrong options ...${env}`);
+}
 
 module.exports = function ( program ) {
   program
     .command('open [entity]')
     .alias('o')
-      // --- po.command('open-one <project>', 'launch a project from tmuxinator config').alias('o')
-      // --- po.command('restart', 're-open sessions in the RESTART list').alias('S')
     .description('launch an entity (single-project or project-group)')
-    // --- .option("-s, --setup_mode [mode]", "Which setup mode to use")
-
-// --- TODO: redefine option: .option('-O, --open-group', 'tbd').alias('O')
-
-    .action(function(env, options){
-      // --- var mode = options.setup_mode || "normal";
-      // --- env = env || 'all';
-      // --- console.log('setup for %s env(s) with %s mode', env, mode);
-      console.log(`Opening ...`);
-    });
+    .option("-p, --project <project_name>", "open/launch a signle project")
+    .option("-g, --project_group <group_name>", "open a group of projects")
+    .option("-r, --restart_group", "open the RESTART group, if it exists")
+    .action(onOpen);
 }
